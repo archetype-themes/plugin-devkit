dbcfe98fc17fdfe414c105eedd48ed9ec0980c48
Thomas Kelly <thomas@archetypethemes.co>
2025-08-07 13:23:57 -0400

    fix: Support comments in JSON (jsonc) (#12)

--- src/utilities/package-json.ts
+++ src/utilities/package-json.ts
@@ -1,3 +1,4 @@
+import { jsonc } from 'jsonc'
 import fs from "node:fs";
 import path from "node:path";
 
@@ -5,7 +6,7 @@ export function getNameFromPackageJson(dir: string): string|undefined {
   const pkgPath = path.join(dir, 'package.json');
   let name;
   if (fs.existsSync(pkgPath)) {
-    const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+    const pkg = jsonc.parse(fs.readFileSync(pkgPath, 'utf8'), { stripComments: true });
     name = pkg.name;
   }
 
@@ -16,7 +17,7 @@ export function getVersionFromPackageJson(dir: string): string|undefined {
   const pkgPath = path.join(dir, 'package.json');
   let version;
   if (fs.existsSync(pkgPath)) {
-    const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+    const pkg = jsonc.parse(fs.readFileSync(pkgPath, 'utf8'), { stripComments: true });
     version = pkg.version;
   }
 
